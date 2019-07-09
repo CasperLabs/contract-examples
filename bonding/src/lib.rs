5e57205fec32a89931bf5879f358a4e64021e635
birchmd <birchmd@casperlabs.io>
2019-07-09 17:30:17 -0400

    EE-464: bonding and unbonding contracts

---
+++ bonding/src/lib.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![feature(alloc)]
+
+#[macro_use]
+extern crate alloc;
+
+extern crate common;
+use common::contract_api::{self, PurseTransferResult};
+use common::contract_api::pointers::UPointer;
+use common::key::Key;
+use common::value::uint::U512;
+
+#[no_mangle]
+pub extern "C" fn call() {
+    let pos_public: UPointer<Key> = contract_api::get_uref("pos").to_u_ptr().unwrap();
+    let pos_contract: Key = contract_api::read(pos_public);
+    let pos_pointer = pos_contract.to_c_ptr().unwrap();
+
+    let source_purse = contract_api::main_purse();
+    let bonding_purse = contract_api::create_purse();
+
+    // Put your desired bonding amount here.
+    // Note: the type is correct, do not change when changing the value.
+    let bond_amount: U512 = 1000.into();
+
+    match contract_api::transfer_from_purse_to_purse(source_purse, bonding_purse, bond_amount) {
+        PurseTransferResult::TransferSuccessful => {
+            let _result: () = contract_api::call_contract(
+                pos_pointer,
+                &("bond", bond_amount, bonding_purse),
+                &vec![Key::URef(bonding_purse.value())],
+            );
+        }
+
+        PurseTransferResult::TransferError => contract_api::revert(1324),
+    }
+}
---
+++ unbonding/src/lib.rs
@@ -0,0 +1,28 @@
+#![no_std]
+#![feature(alloc)]
+
+#[macro_use]
+extern crate alloc;
+
+extern crate common;
+use common::contract_api;
+use common::contract_api::pointers::UPointer;
+use common::key::Key;
+use common::value::uint::U512;
+
+#[no_mangle]
+pub extern "C" fn call() {
+    let pos_public: UPointer<Key> = contract_api::get_uref("pos").to_u_ptr().unwrap();
+    let pos_contract: Key = contract_api::read(pos_public);
+    let pos_pointer = pos_contract.to_c_ptr().unwrap();
+
+    // Put the desired unbonding amount here.
+    // None means that the complete stake will be unbonded, while
+    // Some(x) means an amount x will be unbonded while the rest
+    // remains.
+    // Note the type of the argument is correct, do not change
+    // this when changing the value.
+    let unbond_amount: Option<U512> = None;
+
+    let _result: () = contract_api::call_contract(pos_pointer, &("unbond", unbond_amount), &vec![]);
+}
